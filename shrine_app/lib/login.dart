7fa31a58c1e0b6956d3f09662400c5d8d709a069
salissou.garba-djibo <salissou.garba-djibo@arsium.fr>
2023-11-13 15:20:15 +0100

    Utilisation des thèmes de Material Design (couleur, formes, élévation et type)

--- shrine_app/lib/login.dart
+++ shrine_app/lib/login.dart
@@ -38,7 +38,10 @@ class _LoginPageState extends State<LoginPage> {
               children: <Widget>[
                 Image.asset('assets/diamond.png'),
                 const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Text(
+                  'SHRINE',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
               ],
             ),
             const SizedBox(height: 120.0),
@@ -47,7 +50,7 @@ class _LoginPageState extends State<LoginPage> {
             TextField(
               controller: _usernameController,
               decoration: const InputDecoration(
-                filled: true,
+                //filled: true,
                 labelText: 'Username',
               ),
             ),
@@ -57,11 +60,12 @@ class _LoginPageState extends State<LoginPage> {
             TextField(
               controller: _passwordController,
               decoration: const InputDecoration(
-                filled: true,
+                //filled: true,
                 labelText: 'Password',
               ),
               obscureText: true,
             ),
+            const SizedBox(height: 12.0),
             OverflowBar(
               alignment: MainAxisAlignment.end,
               // TODO: Add a beveled rectangular border to CANCEL (103)
@@ -72,15 +76,27 @@ class _LoginPageState extends State<LoginPage> {
                     _usernameController.clear();
                     _passwordController.clear();
                   },
+                  style: TextButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.secondary,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
+                  ),
                 ),
                 // TODO: Add an elevation to NEXT (103)
                 // TODO: Add a beveled rectangular border to NEXT (103)
                 ElevatedButton(
-                  child: const Text('NEXT'),
-                  onPressed: () {
-                     Navigator.pop(context);
-                  },
+                child: const Text('NEXT'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 8.0,
+                  shape: const BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
                 ),
+              ),
               ],
             ),
           ],
